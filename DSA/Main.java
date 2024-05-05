249825e19e3cc6676b697c47edb30ff985908a5c
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-06 01:26:16 +0530

    Update Main.java

--- DSA/Main.java
+++ DSA/Main.java
@@ -1,10 +1,5 @@
-import java.util.*;
 public class Main{
 public static void main(String [] args){
-Scanner sc= new Scanner(System.in);
-int a= sc.nextInt();
-int b= sc.nextInt();
-int d= a+b;
-System.out.print(d);
+System.out.print("Hello World");
 }
 }
