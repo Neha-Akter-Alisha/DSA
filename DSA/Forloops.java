8a6daf6ae1ee2b0d3f537a9afca0e657a4d942ae
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-06 14:18:53 +0530

    Add files via upload

---
+++ DSA/Forloops.java
@@ -0,0 +1,10 @@
+/*Syntax of For Loop:
+for(intialization,conditions,updations)*/
+//Print the number from 0 to 9.
+public class Forloops{
+    public static void main(String []args) {
+        for(int i = 0; i<10; i++){
+            System.out.println(i);
+        }
+    }
+}
