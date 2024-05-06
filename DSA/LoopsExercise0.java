0c868c73dc04109edda5f1d8b1a0ad1edf519ded
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-06 21:54:32 +0530

    Add files via upload

---
+++ DSA/LoopsExercise0.java
@@ -0,0 +1,13 @@
+//Print the natural n number
+import java.util.*;
+public class LoopsExercise0 {
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+int n =sc.nextInt();
+int sum=0;
+for(int i=0;i<n;i++){
+    sum = sum+i;
+}
+System.out.println(sum);
+}
+}
---
+++ DSA/LoopsExercise1.java
@@ -0,0 +1,11 @@
+//Print the table of a number input by the user.
+import java.util.*;
+public class LoopsExercise1{
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+int n =sc.nextInt();
+for(int i =1; i<11;i++){ ;
+System.out.println(i*n);}
+}
+}
+
