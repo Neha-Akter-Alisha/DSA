efb9e2074095d07d446e97740d88c9b1d4cacf45
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-06 14:34:27 +0530

    Add files via upload

---
+++ DSA/Dowhileloop.java
@@ -0,0 +1,17 @@
+/*Syntax of Do while Loop:
+initialization;
+do{
+    sopln();
+    updation;
+}while(conditions);
+*/
+//Print the number from 0 to 10. */
+public class Dowhileloop {
+    public static void main(String[] args) {
+       int i=0;
+       do{
+        System.out.println(i);
+        i++;
+       } while(i<11);
+    }
+}
---
+++ DSA/Whileloops.java
@@ -0,0 +1,17 @@
+/*while loop syntax
+intialization;
+while(condition){
+    sopln;
+    updation;
+}
+*/
+//Pront numbers from 0 to 10.
+public class Whileloops {
+    public static void main(String[] args) {
+        int i =0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
