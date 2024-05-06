522816890be87e56cee57a5203299ced75246bb9
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-06 11:15:52 +0530

    Add files via upload

---
+++ DSA/Statements0.java
@@ -0,0 +1,14 @@
+//Find out if given age number is odd or even.
+import java.util.*;
+public class Statements0 {
+  public static void main(String [] args){
+  Scanner sc= new Scanner(System.in);
+  int age=sc.nextInt();
+  if(age%2==0){
+    System.out.println("Age is even");
+  } else{
+    System.out.println("Age is odd");
+  }
+  }
+  }  
+
