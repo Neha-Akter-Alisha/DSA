6c1ccb9058a498349b01e40800ca285ff911c80c
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-07 21:52:33 +0530

    Add files via upload

---
+++ DSA/Alphabetcase1.java
@@ -0,0 +1,10 @@
+//Alpha Case check. Input the word and print out what alphabet is in the given index.
+import java.util.*;
+public class Alphabetcase1{
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+char ch=sc.next().trim().charAt(3);
+System.out.println(ch);
+
+}
+}
