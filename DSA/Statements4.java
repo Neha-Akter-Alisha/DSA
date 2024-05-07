b6bd6982a5d0abb1062e5e3f7d5db3477a4e4582
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-07 22:13:44 +0530

    Add files via upload

---
+++ DSA/Statements4.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Statements4{
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+int a = 10;
+int b =20;
+if (a != 9 && b!= 19)//Here we use &&(Bitwise operation) when need to show two conditions are true in one if statement.
+// !=(means not equal to) i.e. if (a != 9 && b!= 19)
+{
+    System.out.println("Hello World");
+}
+}
+}
