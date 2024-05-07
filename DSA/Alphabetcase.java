374ed1b91b58356242639f0d0f4ce056052964cf
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-07 21:43:38 +0530

    Add files via upload

---
+++ DSA/Alphabetcase.java
@@ -0,0 +1,10 @@
+//Alpha Case check
+import java.util.*;
+public class Alphabetcase{
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+//char ch=sc.next().trim().charAt(0);
+System.out.println(sc.next());
+
+}
+}
