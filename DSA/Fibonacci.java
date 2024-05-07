f41edd16d2dbfda866b264a1c82e1103efbf6c50
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-07 22:54:14 +0530

    Add files via upload

---
+++ DSA/Fibonacci.java
@@ -0,0 +1,17 @@
+//FInd out Fibonacci Number
+import java.util.*;
+public class Fibonacci{
+public static void main(String [] args){
+Scanner sc= new Scanner(System.in);
+int n =sc.nextInt();
+int a= 0;
+int b= 1;
+int i= 2;
+while(i<=n){
+  int temp= b;
+  b= b+a;
+  a= temp;
+  i++;
+
+} System.out.println(b);
+}}
