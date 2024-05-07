028f44344c99b4bc8ea5b94f0331279a0bd3ee53
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-07 21:57:50 +0530

    Update Alphabetcase1.java

--- DSA/Alphabetcase1.java
+++ DSA/Alphabetcase1.java
@@ -4,7 +4,7 @@ public class Alphabetcase1{
 public static void main(String [] args){
 Scanner sc= new Scanner(System.in);
 char ch=sc.next().trim().charAt(3);
+//char ch="Hello".trim().charAt(3); (here we already gave the word,so we don't have to input the word)
 System.out.println(ch);
-
 }
 }
