d80815acc512a8ea1611bece6d7633c4a376196f
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-08 15:21:10 +0530

    Add files via upload

---
+++ DSA/Reverse.java
@@ -0,0 +1,16 @@
+package neha;
+import java.util.*;
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int num = sc.nextInt();
+        int ans =0;
+        while(num>0){
+        int rem = num % 10;
+        num/= 10;
+        ans = ans * 10 + rem;
+    }
+    System.out.println(ans);
+
+   }
+}
