3d6f72e639732076f36f268f5839a442bcc6afab
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-14 23:32:12 +0530

    Add files via upload

---
+++ DSA/Pattern6.java
@@ -0,0 +1,14 @@
+//Print the pattern of a Half Pyramid of Numbers.
+public class Pattern6 {
+    public static void main(String[] args) {
+        int n =5;
+
+        //outer loop
+        for(int i=1; i<=n; i++) {
+            for(int j =1; j<=i; j++) {
+                System.out.print(j+" ");
+            }
+   System.out.println();
+  }
+ }
+}
