5687628552905ab7762e817c7fb6ef13a56c8b31
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-14 23:18:51 +0530

    Add files via upload

---
+++ DSA/Pattern4.java
@@ -0,0 +1,15 @@
+//Print the pattern of a Inverted Half Pyramid.
+public class Pattern4 {
+    public static void main(String[] args) {
+        int n =4;
+
+        //outer loop
+        for(int i=n; i>=1; i--) {
+            //inner loop
+            for(int j =1; j<=i; j++) {
+                System.out.print("*");
+        }
+        System.out.println();
+     }
+   }
+ }
