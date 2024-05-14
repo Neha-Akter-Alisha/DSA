e18dbc9393c3df9ae7c54a4fad77d4d3c932f99d
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-14 23:57:46 +0530

    Add files via upload

---
+++ DSA/Pattern10.java
@@ -0,0 +1,24 @@
+//Print the pattern of a Butterfly.
+public class Pattern10 {
+    public static void main(String[] args) {
+        int n =5;
+       
+        //upper half
+        for(int i=1; i<=n; i++) {
+            //1st part
+            for(int j =1; j<=i; j++) {
+                System.out.print(" * ");
+                 }
+                 //spaces
+        int spaces = 2* (n-i);
+        for(int j=1; j<spaces; j++)
+                    System.out.print("  ");
+                 }
+        //2nd part 
+        for(int j =1; j<=i; j++) {
+            System.out.print(" * ");
+             }
+             System.out.println();
+            }
+        
+    }
