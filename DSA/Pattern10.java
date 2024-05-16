2e22e355fb648d99a103e6623ad3c5cc27568324
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-16 23:32:36 +0530

    Update Pattern10.java

--- DSA/Pattern10.java
+++ DSA/Pattern10.java
@@ -1,24 +1,47 @@
 //Print the pattern of a Butterfly.
-public class Pattern10 {
+import java.util.Scanner;
+
+public class Pattern10{
     public static void main(String[] args) {
-        int n =5;
-       
-        //upper half
-        for(int i=1; i<=n; i++) {
-            //1st part
-            for(int j =1; j<=i; j++) {
-                System.out.print(" * ");
-                 }
-                 //spaces
-        int spaces = 2* (n-i);
-        for(int j=1; j<spaces; j++)
-                    System.out.print("  ");
-                 }
-        //2nd part 
-        for(int j =1; j<=i; j++) {
-            System.out.print(" * ");
-             }
-             System.out.println();
-            }
-        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter value of N:");
+        int N = scan.nextInt();
+        scan.close();
+
+        ButterflyPattern obj = new ButterflyPattern();
+        obj.drawButterflyPattern(N);
     }
+}
+
+class Pattern10 {
+    public void drawButterflyPattern(int N) {
+        int space = 2 * N - 1;
+        int star = 0;
+
+        for (int j = 1; j <= 2 * N - 1; j++) {
+            if (j <= N) {
+                space = space - 2;
+                star++;
+            } else {
+                space = space + 2;
+                star--;
+            }
+
+            for (int m = 1; m <= star; m++) {
+                System.out.print("*");
+            }
+
+            for (int n = 1; n <= space; n++) {
+                System.out.print(" ");
+            }
+
+            for (int p = 1; p <= star; p++) {
+                if (p != N) {
+                    System.out.print("*");
+                }
+            }
+
+            System.out.println();
+        }
+    }
+}
