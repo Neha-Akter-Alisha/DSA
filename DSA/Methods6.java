7ee8ff295317f27ebf9eb8e03ad6135bf7884748
Neha Akter Alisha <161963681+Neha-Akter-Alisha@users.noreply.github.com>
2024-05-17 23:33:50 +0530

    Add files via upload

---
+++ DSA/Methods6.java
@@ -0,0 +1,20 @@
+ import java.util.Scanner;
+ public class Methods6 {
+        public static void main(String[] args) {
+            int a, b, c, smallest, temp;
+            Scanner sc = new Scanner(System.in);
+    
+            System.out.println("Enter the first number:");
+            a = sc.nextInt();
+            System.out.println("Enter the second number:");
+            b = sc.nextInt();
+            System.out.println("Enter the third number:");
+            c = sc.nextInt();
+    
+            temp = a < b ? a : b;
+            smallest = c < temp ? c : temp;
+    
+            System.out.println("The smallest number is: " + smallest);
+        }
+    }
+    
